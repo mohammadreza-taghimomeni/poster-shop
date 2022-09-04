5ac71641fca6a532140ffe290009512a8eb219f6
Mohammad Reza Taghimomeni <mrmtm95@outlook.com>
2022-09-04 10:24:31 +0430

    complete adding functionality

--- public/script.js
+++ public/script.js
@@ -1,1 +1,30 @@
-console.log("It works!");
+new Vue({
+	el: "#app",
+	data: {
+		total: 0,
+		products: [
+			{ title: "product 1", id: 1, price: 9.99 },
+			{ title: "product 2", id: 2, price: 9.99 },
+			{ title: "product 3", id: 3, price: 9.99 },
+		],
+		cart: [],
+	},
+	methods: {
+		addToCart: function (product) {
+			this.total += product.price;
+			let found = false;
+			for (let i = 0; i < this.cart.length; i++) {
+				if(this.cart[i].id === product.id){
+					this.cart[i].qty++;
+					found = true;
+				}
+			}
+			if(!found){
+				this.cart.push({
+					...product,
+					qty: 1,
+				});
+			}
+		},
+	},
+});
